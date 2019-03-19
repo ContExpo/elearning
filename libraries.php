58ffbd4ee473b4bf3eb668e92fb09a3e63dc5c4f
Esposito Inchiostro Alessio <alessio.espositoinchiostro@itisbiella.eu>
2019-03-19 13:11:57 +0100

    Aggiunti tema e bozza index

---
+++ libraries.php
@@ -0,0 +1,16 @@
+  <!-- Bootstrap core JavaScript-->
+  <script src="theme/vendor/jquery/jquery.min.js"></script>
+  <script src="theme/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="theme/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="theme/js/sb-admin-2.min.js"></script>
+
+  <!-- Font -->
+  <link href="theme/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="theme/https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="theme/css/sb-admin-2.min.css" rel="stylesheet">

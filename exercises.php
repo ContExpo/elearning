a4787448c732759a9279824c16e44cd3521cdc58
Esposito Inchiostro Alessio <alessio.espositoinchiostro@itisbiella.eu>
2019-04-30 08:41:24 +0200

    fix di aspetto

--- exercises.php
+++ exercises.php
@@ -9,7 +9,30 @@ include_once "functions.php";
             <h3>Storico esercizi</h3>
         </div>
         <div>
-            
+            <?php 
+            $conn=getDBConnection();
+            $sql = "SELECT * FROM exercises WHERE id_user=?";
+            $sql=$conn->prepare($sql);
+            $sql->bind_param("i", $_SESSION["id_user"]);
+            if($sql===false)
+            {
+                die(mysqli_error($conn));
+            }
+            $sql->execute();
+            $result=$sql->get_result();
+            $riga="<tr><td>%d</td><td>%s</td><td>%d</td></tr>";
+            if ($result->num_rows>0)
+            {
+                echo '<table class="table table-striped table-bordered hoverable">';
+                $echo="<thead class='thead-light'><td>ID esercizio</td><td>Data</td><td>Punteggio</td></thead>";
+                $riga="<tr><td>%d</td><td>%s</td><td>%d</td></tr>";
+                while($row=$result->fetch_assoc())
+                {
+                    printf($riga, $row["id_exercise"], $row["date"], $row["score"]);
+                }
+                echo "</table>";
+            }
+            ?>
         </div>
     </div>
 </div>
--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,7 @@
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
       <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center pageLink" data-page="index.php">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center pageLink" data-page="<?php if(!isset($_SESSION["username"])) echo "public_"?>dashboard.php">
         <div class="sidebar-brand-icon rotate-n-15">
           <i class="fas fa-laugh-wink"></i>
         </div>
